a2a03100269671dbf2ae38b574b847f05d632624
Travis Spomer <travis+github@greeneclipsesoftware.com>
2020-04-23 16:32:24 -0700

    Merge multiple input files

--- src/pipeline/config.js
+++ src/pipeline/config.js
@@ -1,52 +1,66 @@
 "use strict"
 
+const _ = require("lodash")
+
 const FluentUIAliases = require("./fluentui-aliases")
 require("./fluentui-shared")
 require("./fluentui-css")
 require("./fluentui-html")
 require("./fluentui-winui")
 
-// TODO: Support merging an arbitrary number of token files before the transformation
+// ------------------------------------------------------------
+// Configure pipeline input and output here
+// ------------------------------------------------------------
 
-const tokensInput = "../tokens/fluentui.json"
-const buildPath = "./build/"
+/*
+	List at least one input JSON file, relative to config.js, not the root of the repo.
+
+	To see the pipeline merge multiple JSON files together, try adding "../tokens/example-red-accent.json" to the array.
+*/
+const inputTokenFiles = ["../tokens/fluentui.json"]
+
+/*
+	Specify the path to where output files should be generated, relative to the root of the repo.
+	Additional folders will be included within that one.
+*/
+const outputPath = "./build/"
+
+// ------------------------------------------------------------
+
+const tokens = {}
+inputTokenFiles.forEach((inputFile) => _.merge(tokens, require(inputFile)))
 
 module.exports = {
-	properties: FluentUIAliases.resolveAliases(require(tokensInput)),
+	properties: FluentUIAliases.resolveAliases(tokens),
 	platforms: {
 		debug: {
 			transformGroup: "js",
-			buildPath: buildPath,
+			buildPath: outputPath,
 			files: [{ destination: "debug/fluentuitokens-debug.json", format: "json" }],
 		},
 
 		reference: {
 			transformGroup: "fluentui/html",
-			buildPath: buildPath,
+			buildPath: outputPath,
 			files: [{ destination: "reference/fluentuitokens.html", format: "fluentui/html/reference" }],
 		},
 
 		css: {
 			transformGroup: "fluentui/css",
-			buildPath: buildPath,
+			buildPath: outputPath,
 			files: [{ destination: "web/fluentuitokens.css", format: "css/variables" }],
 		},
 
 		cssflat: {
 			transformGroup: "fluentui/cssflat",
-			buildPath: buildPath,
+			buildPath: outputPath,
 			files: [{ destination: "web/fluentuitokens-flat.css", format: "css/variables" }],
 		},
 
 		winui: {
 			transformGroup: "fluentui/winui",
-			buildPath: buildPath,
+			buildPath: outputPath,
 			files: [{ destination: "winui/FluentUITokens.xaml", format: "fluentui/xaml/res" }],
 		},
 	},
 }
-
-// ------------------------------------------------------------
-
-// TODO: iOS Swift output will want each control's constants to go into a separate file, so that controls can be
-// shipped as independent packages.
