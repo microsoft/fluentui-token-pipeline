479b9fe1c39ede98ece73d1c23f1a21e5a21cca3
Prasanna Gunuru <prasagu@microsoft.com>
2020-04-23 17:53:13 -0700

    Update config.js
    Added setup to generate swift file.Added transformGroup to format json input to corresponding swift format.
    TODO: Need to fix font format.

--- src/pipeline/config.js
+++ src/pipeline/config.js
@@ -22,6 +22,11 @@ module.exports = {
 			files: [{ destination: 'debug/fluentuitokens-debug.json', format: 'json' }],
 		},
 
+		swift: {
+			transformGroup: 'fluentui/swift',
+			buildPath: buildPath,
+			files: [{ destination: 'web/fluentuitokens.swift', format: 'ios-swift/class.swift', className: 'FluentUITokens' }],
+		},
 		css: {
 			transformGroup: 'fluentui/css',
 			buildPath: buildPath,
@@ -140,6 +145,59 @@ StyleDictionary.registerTransform({
 		console.log(`ERROR: Unable to determine data type based on token name "${prop.path.join('.')}".`)
 	},
 })
+
+
+const getNameForSwift = (path, prefix) =>
+
+const getCGFloatFromNumber = (value) =>
+	parseFloat(value, 10).toFixed(1)
+
+StyleDictionary.registerTransform({
+	name: 'fluentui/name/swift',
+	type: 'name',
+	transformer: (prop, options) => getNameForSwift(prop.path, options.prefix),
+})
+
+StyleDictionary.registerTransform({
+	name: 'fluentui/alias/swift',
+	type: 'value',
+	matcher: (prop) => "resolvedAliasPath" in prop,
+	transformer: (prop, options) => `${getNameForSwift(prop.resolvedAliasPath.split("."), options.prefix)}`,
+})
+
+StyleDictionary.registerTransform({
+	name: 'fluentui/size/swift',
+	type: 'value',
+	matcher: (prop) => prop.attributes.category === 'size',
+	transformer: (prop, options) =>
+	{
+		/*
+			Transforms an array of top/right/bottom/left values into UIEdgeInsets(top: CGFloat, left: CGFloat, bottom: CGFloat, right: CGFloat).
+			Single values are also allowed. All numbers are interpreted as pixels.
+	
+			100
+				-->
+			100.0
+	
+			[ 100, 200, 300, 400 ]
+				-->
+			UIEdgeInsets(top: 100.0, left: 400.0, bottom: 300.0, right: 200.0)
+		*/
+		const value = prop.value
+		if (typeof value === 'number')
+			return `${getCGFloatFromNumber(value)}`
+		else if (Array.isArray(value) && value.length === 4)
+			return `UIEdgeInsets(top: ${getCGFloatFromNumber(value[0])}, left: ${getCGFloatFromNumber(value[3])}, bottom: ${getCGFloatFromNumber(value[2])}, right: ${getCGFloatFromNumber(value[1])})`
+		else
+			console.warn(`Unrecognized size value: "${value}". Use a single number or an array [top, right, bottom, left].`)
+	},
+})
+
+StyleDictionary.registerTransformGroup({
+	name: 'fluentui/swift',
+	transforms: ['fluentui/attribute', 'fluentui/name/swift', 'fluentui/alias/swift', 'time/seconds', 'fluentui/size/swift', 'font/swift/literal', 'color/UIColorSwift'],
+})
+
 
 // ------------------------------------------------------------
 // Output settings: CSS
