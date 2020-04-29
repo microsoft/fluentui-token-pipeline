484af1f5a77c634a6e83291fc80eb866ee01dc5a
Travis Spomer <travis+github@greeneclipsesoftware.com>
2020-04-29 10:48:11 -0700

    Move iOS files into a subfolder

--- src/pipeline/config.js
+++ src/pipeline/config.js
@@ -25,7 +25,7 @@ const inputTokenFiles = ["src/tokens/fluentui.json"]
 	Specify the path to where output files should be generated, relative to the root of the repo.
 	Additional folders will be included within that one.
 */
-const outputPath = "./build/"
+const outputPath = "build"
 
 // ------------------------------------------------------------
 
@@ -37,20 +37,20 @@ module.exports = {
 	platforms: {
 		debug: {
 			transformGroup: "js",
-			buildPath: outputPath,
-			files: [{ destination: "debug/fluentuitokens-debug.json", format: "json" }],
+			buildPath: `${outputPath}/debug/`,
+			files: [{ destination: "fluentuitokens-debug.json", format: "json" }],
 		},
 
 		reference: {
 			transformGroup: "fluentui/html",
-			buildPath: outputPath,
-			files: [{ destination: "reference/fluentuitokens.html", format: "fluentui/html/reference" }],
+			buildPath: `${outputPath}/reference/`,
+			files: [{ destination: "fluentuitokens.html", format: "fluentui/html/reference" }],
 		},
 
 		ios: {
 			transformGroup: 'fluentui/swift',
-			// buildPath to change files directly in fluentUI-tokens-demo iOS app ..//fluentUI-tokens-demo/FluentUITokensDemo/Common/Styles/
-			buildPath: outputPath,
+			// buildPath to change files directly in fluentUI-tokens-demo iOS app: "../fluentUI-tokens-demo/FluentUITokensDemo/Common/Styles/"
+			buildPath: `${outputPath}/ios/`,
 			files: [
 				{ destination: 'FluentUITokens.swift', format: 'ios-swift/class.swift', className: 'FluentUITokens' },
 				{ destination: 'FluentUIColorTokens.swift', format: 'ios-swift/class.swift', className: 'FluentUIColorTokens', filter: 'isColor' },
@@ -61,20 +61,20 @@ module.exports = {
 
 		css: {
 			transformGroup: "fluentui/css",
-			buildPath: outputPath,
-			files: [{ destination: "web/fluentuitokens.css", format: "css/variables" }],
+			buildPath: `${outputPath}/web/`,
+			files: [{ destination: "fluentuitokens.css", format: "css/variables" }],
 		},
 
 		cssflat: {
 			transformGroup: "fluentui/cssflat",
-			buildPath: outputPath,
-			files: [{ destination: "web/fluentuitokens-flat.css", format: "css/variables" }],
+			buildPath: `${outputPath}/web/`,
+			files: [{ destination: "fluentuitokens-flat.css", format: "css/variables" }],
 		},
 
 		winui: {
 			transformGroup: "fluentui/winui",
-			buildPath: outputPath,
-			files: [{ destination: "winui/FluentUITokens.xaml", format: "fluentui/xaml/res" }],
+			buildPath: `${outputPath}/winui/`,
+			files: [{ destination: "FluentUITokens.xaml", format: "fluentui/xaml/res" }],
 		},
 	},
 }
