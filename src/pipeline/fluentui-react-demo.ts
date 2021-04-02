f248cc39dd14b439b4f52fae89c46ede5dac647e
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2021-04-02 16:51:03 -0700

    Add shared color export

--- src/pipeline/fluentui-react-demo.ts
+++ src/pipeline/fluentui-react-demo.ts
@@ -10,7 +10,7 @@ StyleDictionary.registerTransformGroup({
 })
 
 StyleDictionary.registerFormat({
-	name: "fluentui/react/colors",
+	name: "fluentui/react/colors/alias",
 	formatter: (dictionary, config) =>
 	{
 		const c = (path: string) => JSON.stringify((Utils.findPropByPath(path, dictionary.properties.Set) as ValueToken).value)
@@ -209,6 +209,83 @@ export const sharedColorTokens: Record<keyof GlobalSharedColors, SharedColorToke
   platinum: createSharedColorTokens(sharedColors.platinum),
   anchor: createSharedColorTokens(sharedColors.anchor),
   charcoal: createSharedColorTokens(sharedColors.charcoal),
+};
+`)
+	},
+})
+
+StyleDictionary.registerFormat({
+	name: "fluentui/react/colors/global",
+	formatter: (dictionary, config) =>
+	{
+		const c = (path: string) => JSON.stringify((Utils.findPropByPath(path, dictionary.properties.Set) as ValueToken).value)
+
+		const greyRamp = Object.fromEntries(Object.entries<ValueToken>(dictionary.properties.Global.Color.Grey).map(([key, token]) => [key, token.value]))
+
+		const sharedColors = Object.fromEntries(Object.entries<any>(dictionary.properties.Global.Color).map(
+			([key, token]) =>
+			{
+				if (!("Shade50" in token)) return [key, undefined] // Skip things like White and Black and the Grey ramp
+				return [key, Object.fromEntries(Object.entries<any>(token).map(
+					([shadeName, shadeToken]) => [_.lowerFirst(shadeName), shadeToken.value]
+				))]
+			}
+		))
+
+		return (`import { GlobalSharedColors, ColorVariants, ProductBrandColors, BrandVariants } from '../types';
+
+// Global Colors
+// https://www.figma.com/file/KB9oUjMKen2cKnyPG7RgdS/Design-tokens-superset?node-id=1297%3A14745
+// TODO export flat instead of objects for better tree shakeability
+
+export const grey = ${JSON.stringify(greyRamp, undefined, 2)};
+
+export const black = grey[0];
+export const white = grey[100];
+
+export const hyperlink = '#FFFF00';
+export const disabled = '#3FF23F';
+export const selected = '#1AEBFF';
+
+// TODO: values should be set per product theme
+const brand: BrandVariants = {
+  shade60: '#092C47',
+  shade50: '#043862',
+  shade40: '#004578',
+  shade30: '#004C87',
+  shade20: '#005A9E',
+  shade10: '#106EBE',
+  primary: '#0078D4',
+  tint10: '#2899F5',
+  tint20: '#3AA0F3',
+  tint30: '#6CB8F6',
+  tint40: '#C7E0F4',
+  tint50: '#DEECF9',
+  tint60: '#EFF6FC',
+};
+
+// TODO: these colors are not approved yet
+const brandTeams: BrandVariants = {
+  shade60: '#323348',
+  shade50: '#393b5d', // TBD
+  shade40: '#3D3E66',
+  shade30: '#464775',
+  shade20: '#494B83',
+  shade10: '#52558f', // TBD
+  primary: '#6264A7',
+  tint10: '#8f95f8', // TBD
+  tint20: '#9EA2FF',
+  tint30: '#B2B5FF',
+  tint40: '#C7C9FF',
+  tint50: '#DBDCF0',
+  tint60: '#E9EAF6',
+};
+
+export const sharedColors: GlobalSharedColors = ${JSON.stringify(sharedColors, undefined, 2)};
+
+export const brandColors: ProductBrandColors = {
+  teams: brandTeams,
+  web: brand,
 };
 `)
 	},
